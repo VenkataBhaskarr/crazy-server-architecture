369668dc4555f2c2bd4dbdd6eb10b3e8c3adcf94
VenkataBhaskarr <venkatabhaskarcodes@gmail.com>
2023-06-03 16:15:42 +0530

    latest changes updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const [response, setResponse] = useState('Response from server...')
+    const handleClickforCPP = () => {
+        axios.get('http://localhost:8080/cpp').then((res) => {
+            setResponse(res)
+        })
+        console.log("CPP")
+    }
+    const handleClickforRUST = () => {
+        axios.get('http://localhost:8080/cpp').then((res) => {
+            setResponse(res)
+        })
+        console.log("RUST")
+    }
+    const handleClickforGOLANG = () => {
+        axios.get('http://localhost:8080/cpp').then((res) => {
+            setResponse(res)
+        })
+        console.log("GOLANG")
+    }
+    const handleClickforPYTHON = () => {
+        axios.get('http://localhost:8080/cpp').then((res) => {
+            setResponse(res)
+        })
+        console.log("PYTHON")
+    }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="App">
+        <h2>THIS IS SERVERZZ</h2>
+        <div className="button">
+        <button onClick={handleClickforCPP}>
+            Click to get response from C++ server
+        </button >
+        </div>
+        <br/>
+        <div className="button">
+        <button onClick={handleClickforRUST}>
+            Click to get response from RUST server
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        </div>
+        <br/>
+        <div className="button">
+        <button onClick={handleClickforGOLANG}>
+            Click to get response from GOLANG server
+        </button>
+        </div>
+        <br/>
+        <div className="button">
+        <button onClick={handleClickforPYTHON}>
+            Click to get response from PYTHON server
+        </button>
+        </div>
+        <br/>
+        <br/>
+        <div className="response">
+             <h2>{response}</h2>
+        </div>
+    </div>
   )
 }
 
